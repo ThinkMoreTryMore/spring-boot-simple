65f7009ca4d035d45840319809fa2d15d0ced28b
thinkmoretrymore <964108124@qq.com>
2018-04-14 21:53:14 +0800

    # 保存项目

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -2,31 +2,33 @@ package com.example;
 
 import com.example.model.Product;
 import com.example.service.ProductService;
-import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@Transactional
 public class DemoApplicationTests {
 
     @Autowired
     ProductService service;
 
     @Test
+    @Rollback(false)
     public void contextLoads() {
 //        PageInfo<Product> pageinfo = service.selProduct(3, 1);
 //        System.out.print(pageinfo.getList().size()+"11111111111111");
 
         Product pro = new Product();
-        pro.setName("电脑88");
-//        pro.setId(3);
-        service.saveProduct(pro);
-//        service.delProduct(pro);
+        pro.setName("电脑");
+        pro.setPrice(4500f);
+        int  i =  service.saveProduct(pro);
+        System.out.print(i);
     }
 
 }
